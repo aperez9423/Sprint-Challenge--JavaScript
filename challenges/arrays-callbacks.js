d782664d037366adbfefdc2d193b38de3472afb0
Angelica Perez <aperez9423@gmail.com>
2020-05-15 19:24:14 -0400

    completed arrays and callback challenges.

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -50,11 +50,13 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 */
 
+let initialValue = 0;  
 
-const populationTotal = zooAnimals.reduce((total, num, index, array) => {
-    let population = num['population'];
+const populationTotal = zooAnimals.reduce((total, num) => {
+  let population = num['population'];
     total += population;
-}, 0) ;
+    return total;
+}, initialValue) ;
 
 console.log(populationTotal);
 
@@ -68,25 +70,35 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume (a, b, cb) {
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add (num1, num2) {
+  return num1 + num2;
+}
 
+function multiply (num1, num2){
+  return num1 * num2;
+}
+
+function greeting (firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-
-
-
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 /*
 
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+Stretch: If you haven't already, convert your array method callbacks into arrow functions. COMPLETE
 
 */
 
